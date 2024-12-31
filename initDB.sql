9b893553f633016c82056500b130338b4395bb87
Arctic Fox <tyskanhik@mail.ru>
2024-12-31 14:22:20 +0300

    add gh-actions

--- initDB.sql
+++ initDB.sql
@@ -1,49 +1,28 @@
-CREATE database prac WITH OWNER postgres;
-
-\c prac
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-SET ROLE prac;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-create table public.films
-(
-    id          uuid default uuid_generate_v4() not null
-        constraint "PK_697487ada088902377482c970d1"
-            primary key,
-    rating      double precision                not null,
-    director    varchar                         not null,
-    tags        text                            not null,
-    image       varchar                         not null,
-    cover       varchar                         not null,
-    title       varchar                         not null,
-    about       varchar                         not null,
-    description varchar                         not null
+CREATE TABLE public.films (
+    id          uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    rating      double precision                NOT NULL,
+    director    varchar                         NOT NULL,
+    tags        text                            NOT NULL,
+    image       varchar                         NOT NULL,
+    cover       varchar                         NOT NULL,
+    title       varchar                         NOT NULL,
+    about       varchar                         NOT NULL,
+    description varchar                         NOT NULL
 );
 
-alter table public.films
-    owner to prac;
-
-create table public.schedules
-(
-    id       uuid default uuid_generate_v4() not null
-        constraint "PK_7e33fc2ea755a5765e3564e66dd"
-            primary key,
-    daytime  varchar                         not null,
-    hall     integer                         not null,
-    rows     integer                         not null,
-    seats    integer                         not null,
-    price    double precision                not null,
-    taken    text                            not null,
-    "filmId" uuid
-        constraint "FK_1c2f5e637713a429f4854024a76"
-            references public.films
+CREATE TABLE public.schedules (
+    id       uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    daytime  TIMESTAMP                        NOT NULL,
+    hall     integer                         NOT NULL,
+    rows     integer                         NOT NULL,
+    seats    integer                         NOT NULL,
+    price    double precision                NOT NULL,
+    taken    text                            NOT NULL,
+    "filmId" uuid REFERENCES public.films(id) ON DELETE CASCADE
 );
 
-alter table public.schedules
-    owner to prac;
-    
 INSERT INTO films("id","rating","director","tags","image","cover","title","about","description") VALUES('92b8a2a7-ab6b-4fa9-915b-d27945865e39',8.1,'Амелия Хьюз','Рекомендуемые','/bg6s.jpg','/bg6c.jpg','Сон в летний день','Фэнтези-фильм о группе друзей попавших в волшебный лес, где время остановилось.','Причудливый фэнтези-фильм, действие которого происходит в волшебном лесу, где время остановилось. Группа друзей натыкается на это заколдованное царство и поначалу проникается беззаботным духом обитателей, но потом друзьям приходится разойтись. А как встретиться снова, если нет ни времени, ни места встречи?');
 INSERT INTO films("id","rating","director","tags","image","cover","title","about","description") VALUES('0354a762-8928-427f-81d7-1656f717f39c',9.5,'Оливер Беннет','Рекомендуемые','/bg4s.jpg','/bg4c.jpg','Парадокс Нексуса','Фильм об эксперименте по соединению человеческих умов. Исследует вопросы неприкосновенности частной жизни, идентичности и самой природы человеческого сознания','В фильме исследуются последствия новаторского эксперимента по соединению человеческих умов. По мере развития проекта участники сталкиваются с вопросами неприкосновенности частной жизни, идентичности и самой природы человеческого сознания.');
 INSERT INTO films("id","rating","director","tags","image","cover","title","about","description") VALUES('5b70cb1a-61c9-47b1-b207-31f9e89087ff',8.9,'Лила Васкес','Рекомендуемые','/bg2s.jpg','/bg2c.jpg','Стражи Гримуара','Фэнтезийное приключение об истинном значении дружбы, мужества и силы знаний','Захватывающее фэнтезийное приключение, которое рассказывает о группе героев, которые должны защитить древний магический том от попадания в руки тёмного колдуна. История об истинном значении дружбы, мужества и силы знаний.');
